211230237339b8c340f6d43679995f82aae07546
Yuki Kitayama <yk2797@columbia.edu>
2022-12-17 06:00:09 +0000

    added id string parameter to artices API

--- lambda/api-articles/app.py
+++ lambda/api-articles/app.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 import boto3
 import json
 import pprint
@@ -28,9 +29,7 @@ client = MongoClient(host)
 collection = client[DATABASE][COLLECTION]
 
 
-def handler(event, context):
-
-    # Get the metadata of all the articles
+def get_articles():
     body = []
 
     for document in collection.find({}, {'is_featured': 0, 'vote': 0, 'excerpt': 0}):
@@ -40,6 +39,35 @@ def handler(event, context):
         document['like'] = int(document['like']) if 'like' in document else 0
         body.append(document)
 
+    return body    
+
+
+def get_article(id_: str):
+    object_id = ObjectId(id_)
+    document = collection.find_one(
+        {'_id': object_id}, 
+        {'_id': 0, 'excerpt': 0, 'is_featured': 0, 'vote': 0}
+    )
+    document['view'] = int(document['view']) if 'view' in document else 0 
+    document['like'] = int(document['like']) if 'like' in document else 0
+    return document
+
+
+def handler(event, context):
+    
+    # Get a single article metadata by ID
+    if event['queryStringParameters'] and 'id' in event['queryStringParameters']:
+        body = get_article(id_=event['queryStringParameters']['id'])
+        body = json.dumps(body)
+        
+        return {
+            'statusCode': 200,
+            'headers': {'Access-Control-Allow-Origin': '*'},
+            'body': body
+        }
+
+    # Get the metadata of all the articles
+    body = get_articles()
     body = json.dumps(body)
     
     return {
@@ -50,5 +78,12 @@ def handler(event, context):
 
 
 if __name__ == '__main__':
-    event = {}
+    event = {
+        'queryStringParameters': {
+            'id': '639ab1522ec4249ad4cecab5'
+        }
+    }
+    # event = {
+    #     'queryStringParameters': None
+    # }
     pprint.pprint(handler(event, None))
